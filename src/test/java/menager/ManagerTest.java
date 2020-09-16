43e846e2ba2464d355ce316b0ac87c16c7479be9
Sergey K <89184083452.mail.ru>
2020-09-16 22:00:38 +0300

    equals

--- src/test/java/menager/ManagerTest.java
+++ src/test/java/menager/ManagerTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MenagerTest {
-    private Menager menager = new Menager();
+class ManagerTest {
+    private Manager manager = new Manager();
     private Tiket tik1 = new Tiket(1, 5682.35, "VKO", "SIP", 286);
     private Tiket tik2 = new Tiket(2, 5850.89, "DME", "AER", 199);
     private Tiket tik3 = new Tiket(3, 4235.34, "SVO", "MMK", 256);
@@ -15,51 +15,51 @@ class MenagerTest {
 
     @BeforeEach
     public void before() {
-        menager.save(tik1);
-        menager.save(tik2);
-        menager.save(tik3);
-        menager.save(tik4);
+        manager.save(tik1);
+        manager.save(tik2);
+        manager.save(tik3);
+        manager.save(tik4);
     }
 
     @Test
     void searchByFrom_() {
         Tiket[] expented = new Tiket[]{tik2};
-        Tiket[] actual = menager.searchBy("DME", "");
+        Tiket[] actual = manager.searchBy("DME", "");
         assertArrayEquals(expented, actual);
     }
 
     @Test
     void searchByFromOf() {
         Tiket[] expented = new Tiket[]{tik4, tik1};
-        Tiket[] actual = menager.searchBy("BKA", "SIP");
+        Tiket[] actual = manager.searchBy("BKA", "SIP");
         assertArrayEquals(expented, actual);
     }
 
     @Test
     void searchBy_Of() {
         Tiket[] expented = new Tiket[]{tik1};
-        Tiket[] actual = menager.searchBy("", "SIP");
+        Tiket[] actual = manager.searchBy("", "SIP");
         assertArrayEquals(expented, actual);
     }
 
     @Test
     void searchByOfFrom() {
         Tiket[] expented = new Tiket[]{};
-        Tiket[] actual = menager.searchBy("AER", "BKA");
+        Tiket[] actual = manager.searchBy("AER", "BKA");
         assertArrayEquals(expented, actual);
     }
 
     @Test
     void searchBy__() {
         Tiket[] expented = new Tiket[]{};
-        Tiket[] actual = menager.searchBy("", "");
+        Tiket[] actual = manager.searchBy("", "");
         assertArrayEquals(expented, actual);
     }
 
     @Test
     void searchByFromOfOf() {
         Tiket[] expented = new Tiket[]{tik4, tik3, tik2};
-        Tiket[] actual = menager.searchBy("DME", "MMK");
+        Tiket[] actual = manager.searchBy("DME", "MMK");
         assertArrayEquals(expented, actual);
     }
 }
